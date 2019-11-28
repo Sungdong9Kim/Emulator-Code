94811f8533ab58b0cf38f5c01f79205b936a92b1
Sungdong Kim <38519913+Sungdong9Kim@users.noreply.github.com>
2019-11-28 12:56:49 +0900

    Update setup.sh
    Update for modifying minor error.
    (create directory)

--- IoTivity-lite/dimlight/setup.sh
+++ IoTivity-lite/dimlight/setup.sh
@@ -5,8 +5,11 @@ PROJNAME=${PWD##*/}
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/devbuildmake ${CURPWD}/devbuildmake
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-config.json ${CURPWD}/${PROJNAME}-config.json
 cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/dimlight.json ${CURPWD}/${PROJNAME}.json
+# add 'mkdir src' for making directory : if doesn't, pc show 'cannot create regular file' message.
+mkdir src
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight.c ${CURPWD}/src/${PROJNAME}.c
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-main.c ${CURPWD}/src/${PROJNAME}-main.c
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-gen.c ${CURPWD}/src/${PROJNAME}-old.c
-mkdir ${CURPWD}/bin/glade/
+# add -p for making '/bin' directory.
+mkdir -p ${CURPWD}/bin/glade/
 cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/glade/* ${CURPWD}/bin/glade/
