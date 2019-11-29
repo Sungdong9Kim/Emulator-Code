8c9880bc75aae71329003c99dd53546eac14795c
Sungdong Kim <38519913+Sungdong9Kim@users.noreply.github.com>
2019-11-29 11:28:00 +0900

    Update setup.sh

--- IoTivity-lite/dimlight/setup.sh
+++ IoTivity-lite/dimlight/setup.sh
@@ -6,7 +6,7 @@ cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/devbuildmake ${CURPWD}/de
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-config.json ${CURPWD}/${PROJNAME}-config.json
 cp ${EXAMPLEPATH}/Emulator-Code/emulator/dimlight/dimlight.json ${CURPWD}/${PROJNAME}.json
 # add 'mkdir src' for making directory : if doesn't, pc show 'cannot create regular file' message.
-mkdir src
+mkdir ${CURPWD}/src
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight.c ${CURPWD}/src/${PROJNAME}.c
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-main.c ${CURPWD}/src/${PROJNAME}-main.c
 cp ${EXAMPLEPATH}/Emulator-Code/IoTivity-lite/dimlight/dimlight-gen.c ${CURPWD}/src/${PROJNAME}-old.c
